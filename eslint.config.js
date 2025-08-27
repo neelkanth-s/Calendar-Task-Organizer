954ad8bbe79a28c6f1e36c8db97074975550e842
neelkanth-s <109946481+neelkanth-s@users.noreply.github.com>
2025-08-27 15:46:08 -0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
-module.exports = [
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+  // Base recommended rules
+  js.configs.recommended,
+
+  // Ignore unnecessary files
   {
-    ignores: ["**/*.test.js", "**/*.config.js", "**/out/*"],
+    ignores: ["**/*.test.js", "**/*.config.js", "**/out/**"],
   },
+
+  // Custom rules
   {
     rules: {
-      semi: "error",
-      "prefer-const": "error",
+      semi: ["error", "always"], 
+      "prefer-const": "error",   
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], 
     },
   },
 ];
